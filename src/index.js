a62b4b2f034d2150b46c5c3943d8eccaa8be9bd4
Mykola Rudnev <RudnevMykola@gmail.com>
2020-05-28 16:08:26 +0200

    Added graphql to the project

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { MuiThemeProvider, CssBaseline } from '@material-ui/core';
 import App from './App';
+import {ApolloProvider} from '@apollo/react-hooks'
 import theme from './theme';
-
+import client from './graphql/client';
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </MuiThemeProvider>
+    <ApolloProvider client={client}>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </MuiThemeProvider>
+    </ApolloProvider>
    ,document.getElementById('root'));
